afe8c820e3362f1ec0a12b0747c842e4e7bd456d
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2024-09-19 12:35:06 +0100

    Rename track_visits argument

--- lib/refer/controller.rb
+++ lib/refer/controller.rb
@@ -12,12 +12,12 @@ module Refer
       Refer.refer(code: cookies[cookie_name], referee: referee)
     end
 
-    def set_refer_cookie(param_name: Refer.param_name, cookie_name: Refer.cookie_name, code: nil, track_visits: Refer.track_visits)
+    def set_refer_cookie(param_name: Refer.param_name, cookie_name: Refer.cookie_name, code: nil, track_visit: Refer.track_visits)
       code ||= params[param_name]
       return if code.blank?
   
       cookies[cookie_name] = Refer.cookie(code) if Refer.overwrite_cookie || cookies[cookie_name].blank?
-      ReferralCode.find_by(code: code)&.track_visit(request) if track_visits
+      ReferralCode.find_by(code: code)&.track_visit(request) if track_visit
     end
   end
 end
