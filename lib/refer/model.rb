3a09e48d58f21a4033198d9b1fcd46d95a79a604
Chris Oliver <excid3@gmail.com>
2024-11-12 09:24:23 -0600

    Use as: for polymorphic associations

--- lib/refer/model.rb
+++ lib/refer/model.rb
@@ -3,9 +3,9 @@ module Refer
     extend ActiveSupport::Concern
 
     included do
-      has_many :referral_codes, inverse_of: :referrer, class_name: "Refer::ReferralCode", dependent: :destroy
-      has_many :referrals, inverse_of: :referrer, class_name: "Refer::Referral", dependent: :destroy
-      has_one :referral, inverse_of: :referee, class_name: "Refer::Referral", dependent: :destroy
+      has_many :referral_codes, as: :referrer, class_name: "Refer::ReferralCode", dependent: :destroy
+      has_many :referrals, as: :referrer, class_name: "Refer::Referral", dependent: :destroy
+      has_one :referral, as: :referee, class_name: "Refer::Referral", dependent: :destroy
       delegate :referrer, to: :referral
     end
   end
