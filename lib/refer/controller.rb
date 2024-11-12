a6ef61ff3739c2a8e1f6567cc8d20b98e22d384d
Chris Oliver <excid3@gmail.com>
2024-11-12 08:50:13 -0600

    Update controller.rb

--- lib/refer/controller.rb
+++ lib/refer/controller.rb
@@ -13,11 +13,11 @@ module Refer
     end
 
     private
-    
+
     def set_refer_cookie(param_name: Refer.param_name, cookie_name: Refer.cookie_name, code: nil, track_visit: Refer.track_visits)
       code ||= params[param_name]
       return if code.blank?
-  
+
       cookies[cookie_name] = Refer.cookie(code) if Refer.overwrite_cookie || cookies[cookie_name].blank?
       ReferralCode.find_by(code: code)&.track_visit(request) if track_visit
     end
